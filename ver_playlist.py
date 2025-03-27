b0f3c9c55362dcccf22d7eaaa0b783aac3e4dbec
boidepiranha <boidepiranha@gmail.com>
2025-03-26 22:07:28 -0300

    Adicionada funcionalidade para recarregar ao terminar a playlist

--- ver_playlist.py
+++ ver_playlist.py
@@ -1,14 +1,36 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 playlist_id = "PLCcM9n2mu2uHA6fuInzsrEOhiTq7Dsd97"
-iframe_html = f'''
-<iframe width="700" height="394"
-        src="https://www.youtube.com/embed/videoseries?list={playlist_id}"
-        frameborder="0"
-        allow="autoplay; encrypted-media"
-        allowfullscreen>
-</iframe>
-'''
+
+html_code = f"""
+<div id="player"></div>
+
+<script src="https://www.youtube.com/iframe_api"></script>
+<script>
+  let player;
+  function onYouTubeIframeAPIReady() {{
+    player = new YT.Player('player', {{
+      height: '394',
+      width: '700',
+      playerVars: {{
+        listType: 'playlist',
+        list: '{playlist_id}'
+      }},
+      events: {{
+        'onStateChange': onPlayerStateChange
+      }}
+    }});
+  }}
+
+  function onPlayerStateChange(event) {{
+    if (event.data === YT.PlayerState.ENDED) {{
+      console.log("🎬 Playlist terminou. Recarregando...");
+      setTimeout(() => location.reload(), 2000); // Espera 2 segundos e recarrega
+    }}
+  }}
+</script>
+"""
 
 st.title("🎵 Playlist da Batalha")
-st.components.v1.html(iframe_html, height=400)
+components.html(html_code, height=420)
